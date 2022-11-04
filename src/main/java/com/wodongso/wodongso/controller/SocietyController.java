fc0d6a61e6e738e19f38a31d0e57654f630f0866
adx <nogo22@naver.com>
2022-11-05 01:54:58 +0900

    유저 회원가입, 동아리(생성, 수정, 제거)기능 구현, 동아리 프로필 이미지, 배경 이미지 업로드 확인, DB에 URL 삽입

--- src/main/java/com/wodongso/wodongso/controller/SocietyController.java
+++ src/main/java/com/wodongso/wodongso/controller/SocietyController.java
@@ -5,8 +5,11 @@ import com.wodongso.wodongso.service.SocietyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.UUID;
 
 @Controller
 public class SocietyController {
@@ -14,24 +17,70 @@ public class SocietyController {
     @Autowired
     private SocietyService societyService;
 
-
-    @GetMapping("/society/create")
-    public String societyCreate() {
-        return "societyCreate";
-    }
-
+    //    동아리 전체 조회
     @GetMapping("/society/list")
     public String societyList(Model model) {
         model.addAttribute("list", societyService.societyList());
         return "societyList";
     }
 
+    // 특정 동아리 조회
     @GetMapping("/society/detail")
     public String societyDetail(Model model, Integer number) {
         model.addAttribute("society", societyService.societyDetail(number));
         return "societyDetail";
     }
 
-//    public String createSocietyDo(String title, String desc)
+    // 동아리 생성페이지 이동
+    @GetMapping("/society/create")
+    public String societyCreate() {
+        return "societyCreate";
+    }
+
+    //    동아리 생성
+    @PostMapping("/society/createdo")
+    public String createSocietyDo(Society society,
+                                  MultipartFile profileImage,
+                                  MultipartFile backgroundImage) throws Exception {
+        System.out.println("file >> " + profileImage);
+
+        societyService.societyCreate(society, profileImage, backgroundImage);
+
+        return "redirect:/";
+    }
+
+
+    // 동아리 제거
+    @GetMapping("/society/delete")
+    public String societyDelete(Integer number) {
+        System.out.println("numbers >> " + number);
+        societyService.societyDelete(number);
+        return "redirect:/";
+    }
+
+    //    동아리 정보 수정페이지 이동
+    @GetMapping("/society/update/{number}")
+    public String societyUpdate(@PathVariable Integer number, Model model) {
+        model.addAttribute("society", societyService.societyDetail(number));
+
+        //        societyService.societyDelete(number);
+        return "societyUpdate";
+    }
+
+
+    //    동아리 정보 수정
+    @PostMapping("/society/updatedo/{number}")
+    public String societyUpdateDo(@PathVariable Integer number,
+                                  Society society,
+                                  MultipartFile profileImage,
+                                  MultipartFile backgroundImage) throws Exception {
+        Society societyOrigin = societyService.societyDetail(number);
+        societyOrigin.setSimpleDesc(society.getSimpleDesc());
+        societyOrigin.setDetailDesc(society.getDetailDesc());
+        societyOrigin.setPosition(society.getPosition());
+        societyService.societyCreate(societyOrigin, profileImage, backgroundImage);
+        return "redirect:/";
+    }
+
 
 }
--- src/main/java/com/wodongso/wodongso/service/SocietyService.java
+++ src/main/java/com/wodongso/wodongso/service/SocietyService.java
@@ -4,10 +4,12 @@ import com.wodongso.wodongso.entity.Society;
 import com.wodongso.wodongso.repository.SocietyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Service
 public class SocietyService {
@@ -15,13 +17,47 @@ public class SocietyService {
     @Autowired
     private SocietyRepository societyRepository;
 
-    public List<Society> societyList(){
+    public List<Society> societyList() {
         return societyRepository.findAll();
+
+
     }
-    public Society societyDetail(Integer id){
+
+    public Society societyDetail(Integer id) {
         return societyRepository.findById(id).get();
+
+
     }
 
+    public void societyDelete(Integer number) {
+        societyRepository.deleteById(number);
+    }
 
+    public void societyCreate(Society society,
+                              MultipartFile profileImage,
+                              MultipartFile backgroundImage) throws Exception {
+        System.out.println(" p >> " + profileImage.isEmpty());
+        System.out.println(" b >> " + backgroundImage.isEmpty());
+        //        if (profileImage.isEmpty()) {
+        String filePath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+
+        UUID profileUuid = UUID.randomUUID();
+        UUID backgroundUuid = UUID.randomUUID();
+
+        String profileImageName = profileUuid + "_" + profileImage.getOriginalFilename();
+        String backgroundImageName = backgroundUuid + "_" + backgroundImage.getOriginalFilename();
+
+        File saveProfileImage = new File(filePath, profileImageName);
+        File saveBackgroundImage = new File(filePath, backgroundImageName);
+
+        profileImage.transferTo(saveProfileImage);
+        profileImage.transferTo(saveBackgroundImage);
+
+        society.setProfileUrl("/files/" + profileImageName);
+        society.setBackgroundUrl("/files/" + backgroundImageName);
+//        }
+
+        societyRepository.save(society);
+    }
 
 }
