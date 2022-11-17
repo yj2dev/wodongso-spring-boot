3615c0d08804b72dd24ec5d71325183f2dd3e4d9
adu <nogo22@naver.com>
2022-11-17 16:06:08 +0900

    id 중복체크 api 추가

--- src/main/java/com/wodongso/wodongso/controller/UserController.java
+++ src/main/java/com/wodongso/wodongso/controller/UserController.java
@@ -32,6 +32,14 @@ public class UserController {
     @Autowired
     private SocietyService societyService;
 
+
+    // 유저 아이디 중복 확인
+    @GetMapping("/id-check")
+    @ResponseBody
+    public boolean userIsId(String id){
+        return userService.isId(id);
+    }
+
     //    내 동아리 개설 신청 내역 조회
     @GetMapping("/my-create-status")
     public String userMyCreateStatus(Model model, Principal principal) {
--- src/main/java/com/wodongso/wodongso/repository/UserRepository.java
+++ src/main/java/com/wodongso/wodongso/repository/UserRepository.java
@@ -9,5 +9,4 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     User findByIdContaining(String searchKeyword);
 
-
 }
